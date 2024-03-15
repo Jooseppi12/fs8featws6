fc7350bdfa897d4ebda88121c1374be1b52abbf5
Jooseppi12 <urijozsef@europe.com>
2024-03-15 15:30:43 +0100

    Init

---
+++ lib/Library.fs
@@ -0,0 +1,18 @@
+﻿namespace lib
+
+type X =
+    | A
+    | B of X
+
+    static let rec myFunc (x: X) =
+        match x with
+        | A -> printfn "Hello"
+        | B x -> myFunc x
+
+    static let unit : X = A
+
+module Say =
+
+
+    let hello name =
+        printfn "Hello %s" name
